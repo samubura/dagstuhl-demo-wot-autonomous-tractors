975dab49bb1447c1c30bb342a5ccb0710817cc68
Samuele Burattini <samuele.burattini@unibo.it>
2023-12-14 18:01:33 +0100

    Changed script

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
 git submodule init
-git submodule update --remote
-
-cd ./wot-web-simulator
-
-git checkout e5b202a
+git submodule update
 
 cd ../mas-rest-runtime
 
 git submodule init
 git submodule update
-
-git checkout fb38487
-
-cd ../intelliot-hypermas-explorer
-
-git checkout c236af8
-
-cd ..
